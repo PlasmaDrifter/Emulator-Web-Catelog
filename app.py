3a6ad7fac725942c16c1348f3da3949a20bd03c0
PlasmaDrifter <118319137+PlasmaDrifter@users.noreply.github.com>
2026-09-15 11:37:56 -0700

    Release: bump version to v0.6.0 and package standalone binaries

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from werkzeug.utils import secure_filename
 from werkzeug.security import safe_join
 from PIL import Image
 
-__version__ = "0.5.9"
+__version__ = "0.6.0"
 
 if getattr(sys, "frozen", False):
     BUNDLE_DIR = Path(sys._MEIPASS)
