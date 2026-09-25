2445c9f7c8605395d27d1862c366fadbb153495f
PlasmaDrifter <118319137+PlasmaDrifter@users.noreply.github.com>
2026-09-25 14:01:21 -0700

    Release: bump version to v0.7.0 and eliminate tab layout shifts when switching categories

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ from werkzeug.utils import secure_filename
 from werkzeug.security import safe_join
 from PIL import Image
 
-__version__ = "0.6.9"
+__version__ = "0.7.0"
 
 MAX_LOG_ENTRIES = 250
 LOG_BUFFER = deque(maxlen=MAX_LOG_ENTRIES)
